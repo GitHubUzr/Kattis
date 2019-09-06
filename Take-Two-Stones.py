592496d93a44df5adb57ee7856ce0bea2b728e1e
GitHubUzr <49741950+GitHubUzr@users.noreply.github.com>
2019-09-06 16:00:49 -0400

    Add files via upload

---
+++ Take-Two-Stones.py
@@ -0,0 +1,29 @@
+'''
+This python code is meant to take a user input variable 
+and behave as if 2 is continuously subtracted until
+the variable is less than 2, then determine if the
+remainder is even or odd.
+'''
+
+nStonesEven = False
+nStonesOdd = False
+
+# Receive user input
+nStones = int(input())
+
+# Verify user has not entered > 10000000 or < 1
+if nStones > 10000000 or nStones < 1:
+    sys.exit()
+
+# Determine if even or odd
+# 1 automatically qualifies as a remainder that is odd
+# If odd remainder, Alice wins the game
+# If even remainder, Bob wins the game
+if nStones == 1:
+    print("Alice")
+elif nStones % 2 == 0:
+    nStonesEven = True
+    print("Bob")
+else:
+    nStonesOdd = True
+    print("Alice")
