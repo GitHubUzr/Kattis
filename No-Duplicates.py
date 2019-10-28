34bdffc16ddbd4c690bbc24b9e33780762c35901
GitHubUzr <49741950+GitHubUzr@users.noreply.github.com>
2019-10-27 21:11:52 -0400

    Add files via upload

---
+++ No-Duplicates.py
@@ -0,0 +1,28 @@
+# break the line into each word and put into list n
+n = [str(word) for word in input().split()]
+# create a blank list m ; this will all the unique words
+m = []
+# set word repeated to false
+word_repeated = False
+
+# look at each word in the list
+for x in n:
+    # if the word is already in the list
+    if x in m:
+        # set word repeated to true
+        word_repeated = True
+        # and end the loop
+        break
+    # if word is not in the list
+    else:
+        # add it to the list
+        m.append(x)
+        
+# if word repeated was set to True
+# then answer for 'were all the words unique' is 'no'
+if word_repeated:
+    print("no")
+# if word repeated stayed false
+# then answer for 'were all the words unique' is 'yes'
+else:
+    print("yes")
