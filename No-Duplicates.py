1fe8c0ed2b3a303e3c5a2dfea598ca59075e47e0
GitHubUzr <49741950+GitHubUzr@users.noreply.github.com>
2019-10-27 21:37:24 -0400

    Add files via upload

--- No-Duplicates.py
+++ No-Duplicates.py
@@ -3,31 +3,24 @@ if each word is unique and answer no if there
 are duplicates of a word for an inputted string (all caps)
 '''
 
+def no_duplicates(my_list):
+    # compares the number of words th the number of unique words
+    # if total words = unique words, then duplicates is False
+    if len(my_list) == len(set(my_list)):
+        return False
+    # if total words does not equal # unique words, then duplicates is True
+    else:
+        return True
+    
 # break the line into each word and put into list n
 n = [str(word) for word in input().split()]
-# create a blank list m ; this will all the unique words
-m = []
-# set word repeated to false
-word_repeated = False
-
-# look at each word in the list
-for x in n:
-    # if the word is already in the list
-    if x in m:
-        # set word repeated to true
-        word_repeated = True
-        # and end the loop
-        break
-    # if word is not in the list
-    else:
-        # add it to the list
-        m.append(x)
-        
-# if word repeated was set to True
+# calls the function no_duplicates
+my_answer = no_duplicates(n)
+# if duplicates is True
 # then answer for 'were all the words unique' is 'no'
-if word_repeated:
+if my_answer:
     print("no")
-# if word repeated stayed false
+# if duplicates is False
 # then answer for 'were all the words unique' is 'yes'
 else:
     print("yes")
